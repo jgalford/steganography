3263a88a2deba25a77574ea0789dc9d7d9ac1d75
taigercub <tbvandam@anderson.edu>
2023-11-13 14:35:44 -0500

    fixed decode to have cli

--- StegDecode.py
+++ StegDecode.py
@@ -1,5 +1,5 @@
-# StegEncode.py
-# AUTHORS: Johnathan Alford, Dylan Lemon, Jack Long
+# StegDecode.py
+# AUTHORS: Johnathan Alford, Dylan Lemon, Jack Long, Tai Van Dam
 # DATE: 10/6/23
 # PURPOSE: Recover a message from inside the least significant bit(s) of a given image.
 
@@ -7,11 +7,39 @@
 from PIL import Image
 from ast import literal_eval
 from bitarray import bitarray
+import sys #for reading cli inputs
 
 # Array to store extracted binary
 extracted_bin = []
 
-# Open the image and determine size
+#Initialize Variables
+
+if(len(sys.argv)<2):
+    print('''
+    
+  /$$$$$$  /$$$$$$$$ /$$$$$$$$  /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$$ 
+ /$$__  $$|__  $$__/| $$_____/ /$$__  $$ /$$__  $$ /$$__  $$ /$$__  $$| $$__  $$
+| $$  \__/   | $$   | $$      | $$  \__/| $$  \ $$| $$  \__/| $$  \ $$| $$  \ $$
+|  $$$$$$    | $$   | $$$$$   | $$ /$$$$| $$$$$$$$|  $$$$$$ | $$  | $$| $$$$$$$/
+ \____  $$   | $$   | $$__/   | $$|_  $$| $$__  $$ \____  $$| $$  | $$| $$__  $$
+ /$$  \ $$   | $$   | $$      | $$  \ $$| $$  | $$ /$$  \ $$| $$  | $$| $$  \ $$
+|  $$$$$$/   | $$   | $$$$$$$$|  $$$$$$/| $$  | $$|  $$$$$$/|  $$$$$$/| $$  | $$
+ \______/    |__/   |________/ \______/ |__/  |__/ \______/  \______/ |__/  |__/
+                                                                                
+                                                                                
+    
+    Welcome to STEGASOR the stenography tool decoder
+    This program can also be run by inputing the target file to decode after calling the program
+    EG.
+    python3 StegDecode.py /Path/To/Target/File.png
+''')
+    source = input("What file do you want to decode: ")
+else:
+   source = sys.argv[1];
+
+
+
+#Open the image and determine size
 with Image.open("dyr_secret.png") as img:
     width, height = img.size
 
