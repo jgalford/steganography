fa769bb7e1f162ac102d8f95db501910cfbfdda2
rustyshackleford182 <rushackleford182@gmail.com>
2023-11-20 14:49:23 -0500

    GUI works now. Few changes needed maybe.

--- Gooey.py
+++ Gooey.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 from PIL import Image
+from cryptography.fernet import Fernet
+import hashlib
+from base64 import urlsafe_b64encode
+from bitarray import bitarray
 
 window = tk.Tk()
 window.title("Welcome to Stegosaur!")
@@ -9,6 +13,9 @@ window.rowconfigure(0, minsize=230, weight=1)
 window.columnconfigure(1, minsize=400, weight=1)
 
 ende = 'e'
+filename = ''
+mes = tk.StringVar()
+pas = tk.StringVar()
 
 def en_swap():
     global ende
@@ -18,7 +25,7 @@ def en_swap():
         ent_password.grid_forget()
         btn_go.grid_forget()
         lbl_encode.grid(row = 0, column = 0, sticky = "ew", padx = 5, pady = 5)
-        txt_message.grid(row = 0, column = 1, padx = 5, pady = 5)
+        ent_message.grid(row = 0, column = 1, padx = 5, pady = 5)
         lbl_enpassword.grid(row = 1, column = 0, sticky = "ew", padx = 5, pady = 5)
         ent_password.grid(row = 1, column = 1, sticky = "ew", padx = 5, pady = 5)
         btn_go.grid(row = 2, column = 1, padx = 5, pady = 10)
@@ -32,7 +39,7 @@ def de_swap():
         ende = 'd'
         lbl_encode.grid_forget()
         lbl_enpassword.grid_forget()
-        txt_message.grid_forget()
+        ent_message.grid_forget()
         ent_password.grid_forget()
         lbl_depassword.grid(row = 0, column = 0, sticky = "ew", padx = 5, pady = 5)
         ent_password.grid(row = 0, column = 1, sticky = "ew", padx = 5, pady = 5)
@@ -40,31 +47,129 @@ def de_swap():
     else:
         pass
 
+def open():
+    global filename 
+    filename = askopenfilename(
+        title = 'Choose a .png image',
+        filetypes=[('Image files', '*.png')]
+    )
+
+def encrypter(plaintext, password):
+    # Generate hash from password, convert to string
+    hash = hashlib.md5(password.encode()).hexdigest()
+    # Fernet key must be 32 bytes and urlsafe base 64 encoded
+    key = urlsafe_b64encode(hash.encode())
+    token = Fernet(key)
+    ciphertext = token.encrypt(plaintext.encode())
+    #print(ciphertext)
+    return ciphertext.decode()
+
+extracted_bin = []
+
+def decrypter(ciphertext, password):
+    # Generate hash from password, convert to string
+    hash = hashlib.md5(password.encode()).hexdigest()
+    # Fernet key must be 32 bytes and urlsafe base 64 encoded
+    key = urlsafe_b64encode(hash.encode())
+    token = Fernet(key)
+    plaintext = token.decrypt(ciphertext.encode())
+    return plaintext.decode()
+
+def run ():
+    global filename
+    global mes
+    global pas
+    global ende
+
+    i=0
+    message = mes.get()
+    password = pas.get()
+    if ende == 'e':
+        # Encrypt message
+        cipher_message = encrypter(message, password)
+
+        # Convert the message to binary and add a byte(s) at the beginning to indicate how long the message is
+        message_bin = "".join([format(ord(i), "08b") for i in cipher_message])
+        data = bin(int(len(cipher_message)))[2:].zfill(16) + message_bin
+        print(cipher_message)
+        print (len(cipher_message))
+        print(data[0:16])   
+        with Image.open(filename) as img:
+            width, height = img.size
+
+            # Nested loop to target every pixel in the image 
+            for x in range(0, width):
+                for y in range(0, height):
+
+                    # Grab the RGB values at each location
+                       pixel = list(img.getpixel((x, y)))
+                       for n in range(0,3):
+                            if(i < len(data)): # If there is still data to inject, add the data
+                            # ~1 is masking off the last bit so that | will inject the data into the pixel
+                                pixel[n] = pixel[n] & ~1 | int(data[i])
+                                i+=1
+
+                    # Place the new pixel into the correct location
+                            img.putpixel((x,y), tuple(pixel))
+            
+                    # Save the image
+            savename = asksaveasfilename(title = 'Save As...', filetypes=[('Image files', '*.png')], defaultextension = '.png')
+            img.save(savename)
+    else:
+        with Image.open(filename) as img:
+            width, height = img.size
+
+            # Nested loop to target every pixel in the image
+            for x in range(0, width):
+                for y in range(0, height):
+
+                # Grab the RGB values at each location
+                    pixel = list(img.getpixel((x, y)))
+                    for n in range(0,3):
+                # &1 is a bitmask so that only the last pixel is allowed through
+                        extracted_bin.append(pixel[n]&1)
+
+# Get the extracted binary into a string
+
+            data = str(bitarray(extracted_bin).tobytes())
+
+# Chop off first byte, and convert it from binary to integer
+        data_len = str(bitarray(extracted_bin[:16]))
+
+        converted_len = int(data_len[10:-2], 2)
+
+# Weird stuff be happening
+        if (converted_len >= 140):
+            print(decrypter(data[10:converted_len+10], password))
+        else:
+            print(decrypter(data[7:converted_len+7], password))
+
+
 frm_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
 lbl_menu = tk.Label(frm_buttons, text = "Menu")
-btn_open = tk.Button(frm_buttons, text="Open")
+btn_open = tk.Button(frm_buttons, text="Open", command = open)
 btn_encode = tk.Button (frm_buttons, text = "Encode", command = en_swap)
 btn_decode = tk.Button (frm_buttons, text = "Decode", command = de_swap)
-btn_save = tk.Button(frm_buttons, text="Save As...")
+#btn_save = tk.Button(frm_buttons, text="Save As...")
 btn_help = tk.Button(frm_buttons, text = "Help!")
 
 lbl_menu.grid(row = 0, column = 0, sticky = "ew", padx = 5, pady = 5)
 btn_open.grid(row= 1, column=0, sticky="ew", padx=5, pady=10)
 btn_encode.grid(row = 2, column = 0, sticky = "ew", padx = 5, pady = 10)
 btn_decode.grid(row = 3, column = 0, sticky = "ew", padx = 5, pady = 10)
-btn_save.grid(row = 4, column = 0, sticky = "ew", padx = 5, pady = 10)
-btn_help.grid(row = 5, column = 0, sticky = "ew", padx = 5, pady = 10)
+#btn_save.grid(row = 4, column = 0, sticky = "ew", padx = 5, pady = 10)
+btn_help.grid(row = 4, column = 0, sticky = "ew", padx = 5, pady = 10)
 
 frm_secrets = tk.Frame(window, relief = tk.RIDGE, bd = 2)
 lbl_encode = tk.Label (frm_secrets, text = "Enter text to encode: ")
 lbl_enpassword = tk.Label(frm_secrets, text = "Enter password to encrypt: ")
 lbl_depassword = tk.Label(frm_secrets, text = "Enter password to decode: ")
-txt_message = tk.Text(frm_secrets, height = 5, width = 50)
-ent_password = tk.Entry(frm_secrets, width = 50)
-btn_go = tk.Button(frm_secrets, text = "Run", padx = 10, pady = 5)
+ent_message = tk.Entry(frm_secrets, textvariable = mes, width = 50)
+ent_password = tk.Entry(frm_secrets, textvariable = pas, width = 50)
+btn_go = tk.Button(frm_secrets, text = "Run", padx = 10, pady = 5, command = run)
 
 lbl_encode.grid(row = 0, column = 0, sticky = "ew", padx = 5, pady = 5)
-txt_message.grid(row = 0, column = 1, padx = 5, pady = 5)
+ent_message.grid(row = 0, column = 1, padx = 5, pady = 5)
 lbl_enpassword.grid(row = 1, column = 0, sticky = "ew", padx = 5, pady = 5)
 ent_password.grid(row = 1, column = 1, sticky = "ew", padx = 5, pady = 5)
 
